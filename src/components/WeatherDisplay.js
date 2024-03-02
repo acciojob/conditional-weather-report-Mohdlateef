8d286cfae28432c033da89a65cab29804e5caad2
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2024-03-02 16:28:44 +0000

    abc

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -4,6 +4,7 @@ let WeatherDsiplay = ({ weather, condition }) => {
     console.log(condition);
     return (
         <div>
+        testing temperature and color
             {
                 (weather > 20) ? (<div><p>{weather}<span></span></p><p>{condition}</p> </div>) : (<div><p>{weather}<span></span></p><p>{condition}</p></div>)
             }
