5f6521a1a44a89351322e6b90ece1fd3240c76ae
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2024-03-02 15:48:18 +0000

    for

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -5,7 +5,7 @@ console.log(condition);
     return(
         <div>
 {
-(weather>20)?(<div><p>{weather}</p> <p>{condition}</p></div>):(<div><p>{weather}</p><p>{condition}</p></div>)
+(weather>20)?(<div><p>{weather}</p> <spain>{condition}</spain></div>):(<div><p>{weather}</p><spain>{condition}</spain></div>)
 }
         </div>
     )
